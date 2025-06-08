01d04d02b4121779ec81fc641c6dac55f5fb4315
Pratyush Dey <pratyushud@gmail.com>
2025-06-08 21:28:46 +0530

    first commit

---
+++ src/main/java/com/app/pd/ecommerce/config/DataRestConfig.java
@@ -0,0 +1,51 @@
+package com.app.pd.ecommerce.config;
+
+import com.app.pd.ecommerce.entity.Product;
+import com.app.pd.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unSupportedMethods = { HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE };
+        config.getExposureConfiguration().forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unSupportedMethods))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable((unSupportedMethods)));
+
+        config.getExposureConfiguration().forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unSupportedMethods))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable((unSupportedMethods)));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = this.entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<Class>();
+        for(EntityType entity : entities) {
+            entityClasses.add(entity.getJavaType());
+        }
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+}
---
+++ src/main/java/com/app/pd/ecommerce/dao/ProductDAO.java
@@ -0,0 +1,16 @@
+package com.app.pd.ecommerce.dao;
+
+import com.app.pd.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+public interface ProductDAO extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long categoryId, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+}
